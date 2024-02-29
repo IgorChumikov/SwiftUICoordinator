b228b69c483a9a9d11d5d322f76d492d8c2fbb4b
Igor <chumikov.igor@mail.ru>
2024-02-29 15:32:04 +0700

    Комент

--- SwiftUICoordinator/SwiftUICoordinatorApp.swift
+++ SwiftUICoordinator/SwiftUICoordinatorApp.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+// Навигация в Swift UI: что изменилось с появлением iOS 16 https://habr.com/ru/companies/surfstudio/articles/682156/
+
+// Для желающих попользоваться новой навигацией под ios 15 можно использовать https://github.com/johnpatrickmorgan/NavigationBackport с тем же синтаксисом, что и в ios 16
+
 @main
 struct SwiftUICoordinatorApp: App {
     var body: some Scene {
